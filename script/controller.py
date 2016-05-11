8f544059671bc8dee1fe1eb8849689b2211b4124
ROS_Whale <f411024@gmail.com>
2016-05-11 14:35:41 +0800

    corected  the  error  in unit conversion

--- script/controller.py
+++ script/controller.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import roslib; roslib.load_manifest('smart_motor')
-import rospy, serial, smartMotor
+import rospy, serial, smartMotor, math
 from geometry_msgs.msg import Twist
 
 def main():
@@ -24,14 +24,17 @@ def main():
             #get params
             wheleDistance = rospy.get_param("wheleDistance")
             wheleRadius = rospy.get_param("wheleRadius")
-            #calculate speed
+            #calculate speed (rad/s)
             right_whele_speed = ( (2*cmd_twist_x) + (wheleDistance*cmd_twist_r) ) / (2*wheleRadius)
             left_whele_speed  = ( (2*cmd_twist_x) - (wheleDistance*cmd_twist_r) ) / (2*wheleRadius)
+            #make speed (rad/s) to speed (rps)
+            right_whele_speed = right_whele_speed / (2*math.pi)
+            left_whele_speed = left_whele_speed / (2*math.pi)
             #log the speed
             rospy.loginfo("right_whele_speed: %f left_whele_speed:%f r:%f" % (right_whele_speed,left_whele_speed,r))
-            #deliver the speed to Motor
-            m.updateVelocity(_MotorNum= 1,_Acceleration= 1000,_velocity= right_whele_speed)
-            m.updateVelocity(_MotorNum= 2,_Acceleration= 1000,_velocity= left_whele_speed)
+            #deliver the speed to Motor (1 rps = 60 rpm = 32768)
+            m.updateVelocity(_MotorNum= 1,_Acceleration= 1000,_velocity= right_whele_speed * 32768)
+            m.updateVelocity(_MotorNum= 2,_Acceleration= 1000,_velocity= left_whele_speed * 32768)
         else:
             rospy.logerr("Please input param 'wheleDistance' or 'wheleRadius'")
 
